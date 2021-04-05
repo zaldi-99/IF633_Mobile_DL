60acf196f5a829a2cb97d89ddeea44866aa7c1fe
zaldi-99 <rizkyzaldi@gmail.com>
2021-04-05 08:27:52 +0700

    week8

---
+++ week08/app/src/main/java/umn/ac/id/week08/MainActivity.java
@@ -0,0 +1,48 @@
+package umn.ac.id.week08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnTutor1, btnTutor2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnTutor1 = findViewById(R.id.btnTutorial1);
+        btnTutor2 = findViewById(R.id.btnTutorial2);
+
+        btnTutor1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToTutorial1();
+            }
+        });
+
+        btnTutor2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToTutorial2();
+            }
+        });
+        
+    }
+    private void goToTutorial1() {
+        Intent intent = new Intent(this, Tutorial1.class);
+        startActivity(intent);
+
+    }
+
+    private void goToTutorial2() {
+        Intent intent = new Intent(this, Tutorial2.class);
+        startActivity(intent);
+    }
+
+
+}
